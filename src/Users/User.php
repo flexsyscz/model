ec9fabbeb6b58fed158f376f31b762acd5151e7a
Václav Lukeš <vasek@flexsys.cz>
2025-01-19 19:55:17 +0100

    added users support

---
+++ src/Users/User.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Flexsyscz\Model\Users;
+
+use Flexsyscz\Model\Entity;
+
+
+abstract class User extends Entity
+{
+}
---
+++ src/Users/UsersFacade.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Flexsyscz\Model\Users;
+
+use Flexsyscz\Model\Facade;
+
+
+abstract class UsersFacade extends Facade
+{
+}
